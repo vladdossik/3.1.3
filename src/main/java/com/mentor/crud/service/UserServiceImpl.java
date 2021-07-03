95a3fc01194d95979300d43031f988f16676ccc9
vladdosiik <vladdosiik7540@gmail.com>
2021-07-03 15:05:13 +0300

    update

--- src/main/java/com/mentor/crud/service/UserServiceImpl.java
+++ src/main/java/com/mentor/crud/service/UserServiceImpl.java
@@ -6,11 +6,8 @@ import com.mentor.crud.repo.RoleRepository;
 import com.mentor.crud.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -39,7 +36,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public Role showRole(int id) {
         Optional<Role> optionalUser = roleRepository.findById(id);
-
         return optionalUser.get();
     }
 

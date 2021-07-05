2a450d27babec2c9133e8eaca0f926414ea8ea32
vladdosiik <vladdosiik7540@gmail.com>
2021-07-05 13:18:06 +0300

    done

--- src/main/java/com/mentor/crud/model/User.java
+++ src/main/java/com/mentor/crud/model/User.java
@@ -32,7 +32,7 @@ public class User implements UserDetails {
     private String password;
 
 
-    @OneToMany(fetch = FetchType.EAGER)
+    @OneToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
 

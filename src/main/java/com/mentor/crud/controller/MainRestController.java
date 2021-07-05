23d5997dfb11c2e725a76ddbc246c815eb43cacb
vladdosiik <vladdosiik7540@gmail.com>
2021-07-05 13:17:41 +0300

    done

--- src/main/java/com/mentor/crud/controller/MainRestController.java
+++ src/main/java/com/mentor/crud/controller/MainRestController.java
@@ -3,6 +3,8 @@ package com.mentor.crud.controller;
 import com.mentor.crud.model.User;
 import com.mentor.crud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,29 +16,38 @@ public class MainRestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    public MainRestController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/allusers")
-    public List<User> list() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> list() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public User getOne(@PathVariable Integer id) {
-        return userService.show(id);
+    public ResponseEntity<User> getOne(@PathVariable Integer id) {
+        User user = userService.show(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping("/newUser")
-    public User addUserBd(@RequestBody User user) {
-        return userService.save(user);
+    public ResponseEntity<String> addUserBd(@RequestBody User user) {
+        userService.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/edit")
-    public User update(@RequestBody User user) {
-        return userService.update(user);
+    public ResponseEntity<String> update(@RequestBody User user) {
+        userService.update(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable("id") Integer id) {
+    public ResponseEntity<String> delete(@PathVariable("id") Integer id) {
         userService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
